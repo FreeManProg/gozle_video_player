df66c9cfb3055849f22caa55142308afd0e551c0
mainpanel <109960775+mainpanel@users.noreply.github.com>
2023-04-15 01:33:48 +0500

    fix

--- lib/src/core/better_player_with_controls.dart
+++ lib/src/core/better_player_with_controls.dart
@@ -349,7 +349,11 @@ class _BetterPlayerVideoFitWidgetState
             height: double.infinity,
             child: FittedBox(
               fit: widget.boxFit,
-              child: VideoPlayer(controller),
+              child: SizedBox(
+                width: controller!.value.size?.width ?? 0,
+                height: controller!.value.size?.height ?? 0,
+                child: VideoPlayer(controller),
+              ),
             ),
           ),
         ),
