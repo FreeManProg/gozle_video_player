c6dd2e0c597ef79b5a62f9be768fc014042cb291
mainpanel <109960775+mainpanel@users.noreply.github.com>
2023-04-16 18:08:20 +0500

    update

--- lib/src/core/better_player_with_controls.dart
+++ lib/src/core/better_player_with_controls.dart
@@ -101,51 +101,15 @@ class _BetterPlayerWithControlsState extends State<BetterPlayerWithControls> {
     }
 
     aspectRatio ??= 16 / 9;
-    final bool isPinchToZoomEnabled =
-        betterPlayerController.betterPlayerConfiguration.enablePinchToZoom;
+    final bool isPinchToZoomEnabled = betterPlayerController.isFullScreen;
 
     final innerContainer = Container(
       width: double.infinity,
       color: betterPlayerController
           .betterPlayerConfiguration.controlsConfiguration.backgroundColor,
-      child: GestureDetector(
-        onScaleUpdate: !isPinchToZoomEnabled
-            ? null
-            : (details) {
-                // calculating difference between new scale and previous value
-                // when zooming out, a factor of 2 is used so that the zoom value can be reduced to 0
-                // when zooming in, a factor of 2 is used to uniformly change the zoom value
-                double diff = (details.scale - scaleListener.value) * 2;
-
-                // checking current zoom for maximum and minimum value
-                // minimum value is 0 - video not showing on notch area
-                // maximum value is 1 - video showing on notch area
-                if (zoomListener.value + diff > 1) {
-                  zoomListener.value = 1;
-                } else if (zoomListener.value + diff < 0) {
-                  zoomListener.value = 0;
-                } else {
-                  zoomListener.value += diff;
-                }
-                // set current scale as scale value
-                scaleListener.value = details.scale;
-              },
-        onScaleEnd: !isPinchToZoomEnabled
-            ? null
-            : (details) {
-                // bringing zoom value to maximum or minimum
-                if (zoomListener.value <= 0.5) {
-                  zoomListener.value = 0;
-                } else {
-                  zoomListener.value = 1;
-                }
-                // set default scale value
-                scaleListener.value = 1;
-              },
-        child: AspectRatio(
-          aspectRatio: aspectRatio,
-          child: _buildPlayerWithControls(betterPlayerController, context),
-        ),
+      child: AspectRatio(
+        aspectRatio: aspectRatio,
+        child: _buildPlayerWithControls(betterPlayerController, context),
       ),
     );
 
